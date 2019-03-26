6634add2254c0854741b4d689db2a73c3063cce5
Oli W <oli@oliverwilkie.com>
2019-03-26 08:58:52 -0700

    add griddler

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,7 @@ require 'sidekiq/web'
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   mount Sidekiq::Web => '/sidekiq'
+
+  # mount using default path: /email_processor
+  mount_griddler
 end
