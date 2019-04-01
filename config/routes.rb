229733e927417b4e734eaaa1d0d3907e98ad1e26
Oli W <oli@oliverwilkie.com>
2019-03-31 20:29:08 -0700

    Added a custom root

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 require 'sidekiq/web'
 
 Rails.application.routes.draw do
+  root 'welcome#index'
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   mount Sidekiq::Web => '/sidekiq'
 
