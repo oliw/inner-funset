ebaf73b562450a6f3c2410b2ea82a9d01a286ec2
Oli W <oli@oliverwilkie.com>
2019-04-13 22:49:59 +0900

    Add Webpack

---
+++ postcss.config.js
@@ -0,0 +1,15 @@
+var tailwindcss = require("tailwindcss");
+
+module.exports = {
+  plugins: [
+    require("postcss-import"),
+    tailwindcss("./app/javascript/css/tailwind.js"),
+    require("postcss-flexbugs-fixes"),
+    require("postcss-preset-env")({
+      autoprefixer: {
+        flexbox: "no-2009"
+      },
+      stage: 3
+    })
+  ]
+};
