6fbadd9e9436638c60f204097cdcda1da957dcbe
Oli W <oli@oliverwilkie.com>
2019-04-13 20:43:24 +0900

    Fix missing no-reply@ to unsubscribe

--- app/mailers/unsubscribe_mailer.rb
+++ app/mailers/unsubscribe_mailer.rb
@@ -1,4 +1,6 @@
 class UnsubscribeMailer < ApplicationMailer
+  default from: 'no-reply@inner-funset.com'
+  
   def success
     @neighbor = params[:neighbor]
     mail(to: @neighbor.email, subject: 'You have been unsubscribed')
