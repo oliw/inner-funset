22f23028652a1bf023271033f8304d3f88847289
Oli W <oli@oliverwilkie.com>
2019-04-13 20:48:34 +0900

    Use sendgrid for outgoing mail

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,6 +65,16 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
+  config.action_mailer.smtp_settings = {
+  :user_name => ENV.fetch('SENDGRID_USERNAME'),
+  :password => ENV.fetch('SENDGRID_PASSWORD'),
+  :domain => 'innerfunset.com',
+  :address => 'smtp.sendgrid.net',
+  :port => 587,
+  :authentication => :plain,
+  :enable_starttls_auto => true
+}
+
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
