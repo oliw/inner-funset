121eefd1d10fefc5198102357cba66fe611b36d5
Oli W <oli@oliverwilkie.com>
2019-06-13 22:35:06 -0700

    Email threads

---
+++ app/mailers/match_mailer.rb
@@ -0,0 +1,18 @@
+class MatchMailer < ApplicationMailer
+  default from: 'match@innerfunset.com'
+
+  def match
+    @match = params[:match]
+    @neighbor = params[:neighbor]
+    reply_to = "match-#{@match.id}@innerfunset.com"
+    mail(to: @neighbor.email, subject: 'Its an Inner Sunset Match!', reply_to: reply_to)
+  end
+
+  def forwarder
+    @match = params[:match]
+    @neighbor = params[:neighbor]
+    @body = params[:body]
+    reply_to = "match-#{@match.id}@innerfunset.com"
+    mail(to: @neighbor.email, subject: 'Its an Inner Sunset Match!', reply_to: reply_to)
+  end
+end
--- app/models/email_processor.rb
+++ app/models/email_processor.rb
@@ -6,5 +6,23 @@ class EmailProcessor
 
   def process
     Rails.logger.info "Email received!"
+    from = @email.from[:email]
+    to = @email.to[0][:email]
+    body = @email.body
+    neighbor = Neighbor.find_by_email(from)
+    return if neighbor.blank?
+    regex_matches = /match-(.*)@.*/i.match(to)
+    return if regex_matches.blank?
+    match_id = regex_matches.captures[0].to_i
+    match = Match.joins(:neighbors).where(neighbors: {id: neighbor.id}).find_by(id: match_id)
+    return if match.blank?
+
+    other_neighbors = match.neighbors.select do |other_neighbor|
+      other_neighbor.id != neighbor.id
+    end
+
+    other_neighbors.each do |other_neighbor|
+      MatchMailer.with(match: match, neighbor: other_neighbor, body: body).forwarder.deliver_now
+    end
   end
 end
